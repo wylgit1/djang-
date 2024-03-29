a605adfc0336c9209f5df58b34a3abd5ba5eb089
wylgit1 <2322767536@qq.com>
2024-03-29 20:05:55 +0800

    修改model.py

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
-	uname = models.CharField(max_length=20)
+	uname = models.CharField(max_length=25)
 	upwd = models.CharField(max_length=40)
